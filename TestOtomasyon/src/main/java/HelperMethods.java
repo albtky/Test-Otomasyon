03573296096601f28725cde18975cce8fbe99705
Alperen Tokay <alperen.tky06@gmail.com>
2025-08-19 19:08:18 +0300

    Basic otomasyon komutları yazıldı.

---
+++ TestOtomasyon/src/main/java/HelperMethods.java
@@ -0,0 +1,59 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.time.Duration;
+
+public class HelperMethods {
+    WebDriver driver;
+
+    public HelperMethods(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void driverGet(String URL)
+    {
+        driver.get(URL);
+    }
+    public void clickWhenClickable(By locator){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
+    }
+    public void txtInput(By locator, String text){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        wait.until(ExpectedConditions.elementToBeClickable(locator)).sendKeys(text);
+
+    }
+
+    public void sleep(int seconds) throws InterruptedException {
+        Thread.sleep(seconds);
+    }
+
+    public void checkBasket(int seconds) throws InterruptedException {
+        Thread.sleep(seconds);
+    }
+
+    public void productCheckInList(By locator_Amount,
+                                   By locator_productDetail,
+                                   By locator_product_title)
+    {
+        //kaç tane ürün var onu string içerisinden ayrıştır
+        // for döngüsüne ekle
+        //tek tek ürünlerin içerisine gir ve
+        // title'larında aranan (class = xeL9CQ3JILmYoQPCgDcl) isim var mı kontrol et
+
+        WebElement productAmount =  driver.findElement(locator_Amount);
+        for (int i = 0; i < ; i++) {
+            
+        }
+        for (int i = 0; i < Integer.parseInt(null); i++) {
+
+        }
+    }
+
+
+
+}
--- TestOtomasyon/src/test/java/LoginTests.java
+++ TestOtomasyon/src/test/java/LoginTests.java
@@ -1,31 +1,85 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.time.Duration;
 
 public class LoginTests {
+    WebDriver driver;
+    @BeforeEach
+    public void beforeEach() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        options.addArguments("start-maximized");
+        options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139 Safari/537.36");
 
+        driver = new ChromeDriver(options); // önce driver başlat
+        driver.manage().window().maximize(); // sonra tam ekran yap
 
+    }
     @Test
-    void hbLoginTest()
-    {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
+     void hbLoginTest() throws InterruptedException {
+        HelperMethods helpers = new HelperMethods(driver); // 🔁 driver geçildi
 
-        driver.get("https://www.hepsiburada.com");
+        helpers.driverGet("https://www.hepsiburada.com");
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        helpers.clickWhenClickable(By.id("myAccount"));
+        helpers.sleep(6000);
 
-        WebElement accountBtn = wait.until(ExpectedConditions.elementToBeClickable(By.id("myAccount")));
-        accountBtn.click();
+        helpers.clickWhenClickable(By.id("login"));
+        helpers.sleep(2000);
 
+        helpers.clickWhenClickable(By.id("txtUserName"));
+        helpers.txtInput(By.id("txtUserName"), "alperen.tky06@gmail.com");
+        helpers.sleep(2000);
 
-        WebElement loginBtn  = wait.until(ExpectedConditions.elementToBeClickable(By.id("login")));
-        loginBtn.click();
+        helpers.clickWhenClickable(By.id("txtPassword"));
+        helpers.txtInput(By.id("txtPassword"), "Tek1getir1");
+        helpers.sleep(2000);
+
+        helpers.clickWhenClickable(By.id("btnLogin"));
+        helpers.sleep(2000);
+
+        helpers.clickWhenClickable(By.id("myAccount"));
+        helpers.sleep(2000);
+
+        helpers.clickWhenClickable(By.xpath("//div[@class='sf-OldMyAccount-IMCbeURaRPyXfubeWCC7']//a[.='Siparişlerim']"));
+        helpers.sleep(2000);
+
+        helpers.clickWhenClickable(By.xpath("//small[.='Sipariş no:452 525 812 5']"));
+        helpers.sleep(2000);
+
+        helpers.clickWhenClickable(By.xpath("//a[.='Zippo Benzin']"));
+        helpers.sleep(2000);
+
+        helpers.clickWhenClickable(By.xpath("//button[@class='sf-Axjyr hWFMEy satldmja8yv z8QiDixMPSOOi87wSJ1D']/i[1]"));
+        helpers.sleep(2000);
+
+        helpers.clickWhenClickable(By.xpath("//button[.='Sepete git']"));
+        helpers.sleep(2000);
+
+        helpers.clickWhenClickable(By.cssSelector("[aria-label='Ürünü Azalt'] > [width='32']"));
+        //Ürünü sepetten çıkartmak istediğinize emin misiniz uyarısı !!
+        helpers.clickWhenClickable(By.cssSelector("[aria-label='Ürünü Azalt'] > [width='32']"));
+        helpers.sleep(6000);
+
+        helpers.clickWhenClickable(By.id("continue_step_btn"));
+        helpers.sleep(2000);
+
+        String successHtml = "<html><body style='display:flex;justify-content:center;align-items:center;height:100vh;background:white;font-family:sans-serif;'>" +
+                "<h1 style='color:green;'>:))))) All of the test is successfully resulted in success</h1>" +
+                "</body></html>";
+
+        driver.get("data:text/html," + successHtml);
+        helpers.sleep(8000);
+
+    }
+
+    @AfterEach
+    public void afterEach() {
+        driver.quit();
     }
 }
